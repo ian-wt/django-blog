0668c67608b22a39db91cef69d0da354f0387848
Ian Waldron <ian.waldron@scribawaldron.com>
2025-01-30 15:21:50 -0800

    start basic admin view

--- app/admin/urls.py
+++ app/admin/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from .views.dashboard import DashboardTemplateView
+
+
+app_name = 'admin'
 
 urlpatterns = [
-
+    path('', DashboardTemplateView.as_view(), name='dashboard'),
 ]
