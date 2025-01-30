975866bab434f8062f54fb6b5e79408854c31e62
Ian Waldron <ian.waldron@scribawaldron.com>
2025-01-30 09:25:33 -0800

    add / fix tests
    add test for cover_image_file_path function and fix required for publish test that needed a deep copy of a dictionary

--- app/core/tests/test_models/test_blog.py
+++ app/core/tests/test_models/test_blog.py
@@ -1,14 +1,20 @@
 import datetime
+import uuid
+from uuid import UUID
+from copy import deepcopy
 
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 from django.utils.text import slugify
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from faker import Faker
 
 from core.tests.factories import UserFactory
 from core.models import Post
+from core.models.blog import cover_image_file_path
+
 
 fake = Faker()
 
@@ -19,7 +25,7 @@ class TestPost(TestCase):
         cls.required_for_publish = {
             'description': fake.paragraph(nb_sentences=2),
             'body': fake.paragraphs(nb=5),
-            'cover_image': fake.file_name('image', 'jpc'),
+            'cover_image': fake.file_name('image', 'jpg'),
             'cover_image_alt': fake.sentence(nb_words=5)
         }
 
@@ -47,15 +53,16 @@ class TestPost(TestCase):
         # use missing description
         # set all other fields required to publish
         missing_field = 'description'
-        incomplete_fields = self.required_for_publish
+        incomplete_fields = deepcopy(self.required_for_publish)
         incomplete_fields.pop(missing_field)
-        for k, v in self.required_for_publish.items():
+
+        for k, v in incomplete_fields.items():
             setattr(self.blog_post, k, v)
 
         with self.assertRaisesMessage(
             ValidationError,
             f'{missing_field.replace("_", " ").title()} is required to publish.'
-        ):
+        ) as cm:
             # set published
             self.blog_post.published_at = timezone.now()
             self.blog_post.save()
@@ -118,3 +125,48 @@ class TestPost(TestCase):
         self.assertEqual(self.blog_post.meta_description, meta_description)
         self.assertEqual(self.blog_post.meta_title_display, meta_title)
         self.assertEqual(self.blog_post.meta_description_display, meta_description)
+
+    def test_cover_image_file_path_function(self):
+        required_items = deepcopy(self.required_for_publish)
+        file_name = required_items.pop('cover_image')
+        for k, v in required_items.items():
+            setattr(self.blog_post, k, v)
+
+        self.blog_post.cover_image = SimpleUploadedFile(
+            file_name,
+            b'fake image bytes.'
+        )
+
+        self.assertEqual(
+            file_name,
+            self.blog_post.cover_image
+        )
+        self.blog_post.save()
+
+        def _is_valid_uuid(value: str) -> bool:
+            # make sure to match uuid verion in tested function
+            # make sure a string is pased otherwise you'll get a TypeError
+            #   raised and not a ValueError as expected
+            try:
+                uuid_obj = uuid.UUID(value, version=4)
+                return str(uuid_obj) == value
+            except ValueError:
+                return False
+
+        # to verify method works
+        self.assertFalse(_is_valid_uuid('not-uuid'))
+
+        # split off path prefix
+        path_prefix ,processed_file_name = self.blog_post.cover_image.name.split('/')
+
+        # lose the extension
+        no_ext = processed_file_name.split('.')[0]
+
+        # test for uuid formatting
+        self.assertTrue(_is_valid_uuid(no_ext))
+
+        # test path prefix
+        self.assertEqual(
+            path_prefix,
+            'post-cover-images'
+        )
